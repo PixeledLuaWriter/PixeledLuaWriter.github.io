06b2b4e4963d1a61df21876a217fe4192965c531
PixeledLuaWriter <78665021+PixeledLuaWriter@users.noreply.github.com>
2021-03-05 19:56:21 -0500

    Update CreateTween.lua

--- Roblox_Modules/CreateTween.lua
+++ Roblox_Modules/CreateTween.lua
@@ -2,17 +2,18 @@
 --[[ Author: Godcat567 ]]
 
 --[[
-        USAGE: require(script:WaitForChild("Create_Tween"))
+				USAGE: require(script:WaitForChild("Create_Tween"))
 
-        @params :CreateNewTween(Joint or part, {Tween Data}, EasingStyle, EasingDirection, time, PlayOnCreation)
+				@params :CreateNewTween(Joint or part, {Tween Data}, EasingStyle, EasingDirection, time, PlayOnCreation)
+
 ]]
 
 --[[ Variable Definition ]]
 
 local ServiceIndex = setmetatable({}, {
-   __index = function(self, callback)
-       return pcall(game.GetService, callback)
-   end
+		__index = function(self, callback)
+			return pcall(game.GetService, callback)
+		end
 })
 
 local TweenIndex = {}
@@ -21,20 +22,20 @@ local TweenServ = ServiceIndex.TweenService
 --[[ Function Definition ]]
 
 function TweenIndex:CreateNewTween(Joint, TweenData, EasingStyle, EasingDirection, time, PlayOnCreation)
-  local info = TweenInfo.new(
-    time / 1,
-    Enum.EasingStyle[EasingStyle],
-    Enum.EasingDirection[EasingDirection],
-    0,
-    false,
-    0
-  )
-  local tween = TweenServ:Create(Joint, info, TweenData)
-  if(PlayOnCreation)then
-    tween:Play()
-    else
-    return tween
-  end
+	local info = TweenInfo.new(
+		time / 1,
+		Enum.EasingStyle[EasingStyle],
+		Enum.EasingDirection[EasingDirection],
+		0,
+		false,
+		0
+	)
+	local tween = TweenServ:Create(Joint, info, TweenData)
+	if(PlayOnCreation)then
+		tween:Play()
+	else
+		return tween
+	end
 end
 
 --[[ Return Data ]]
