1fe9fb32d308b9e430856ed30df15298be246e6c
PixeledLuaWriter <78665021+PixeledLuaWriter@users.noreply.github.com>
2021-03-20 19:30:14 -0400

    Create MetaModule.lua

---
+++ Assets/Roblox_Modules/MetaModule/MetaModule.lua
@@ -0,0 +1,66 @@
+--[[ FileName: MetaModule.lua ]]
+--[[ Author: Godcat567 ]]
+
+--[[ Variable Definition ]]
+
+local Meta = {}
+local MetaScripts = {}
+local Players = game:service'Players'
+
+--[[ Script Archiving ]]
+
+for index, child in next, script:GetChildren() do
+	MetaScripts[child.Name] = child:Clone();
+	child:Destroy()
+end
+
+--[[ Function Definition ]]
+
+function GetPlayerByName(User)
+	for index, child in next, Players:GetPlayers() do
+		if(tostring(child) == User) then
+			return child
+		end
+	end
+	return nil;
+end
+
+function GetPlayerByUserId(User)
+	local success, callback = pcall(Players.GetNameFromUserIdAsync, Players, User)
+	if success then
+		return Players[callback]
+	else
+		error(callback)
+	end
+end
+
+--[[ Metatable Settings ]]
+
+setmetatable(Meta, {
+	__index = function(self, index)
+		local scripts = MetaScripts[index]
+		if(scripts)then
+			return function(ThePlayerItself)
+				if typeof(ThePlayerItself) == "string" then
+					local Player = GetPlayerByName(tostring(ThePlayerItself))
+					if(Player and Player:FindFirstChildOfClass'PlayerGui')then
+						scripts.Parent = Player:FindFirstChildOfClass'PlayerGui'
+						scripts.Disabled = false
+					end
+				elseif typeof(ThePlayerItself) == "number" then
+					local Player = GetPlayerByUserId(tostring(ThePlayerItself))
+					if(Player and Player:FindFirstChildOfClass'PlayerGui')then
+						scripts.Parent = Player:FindFirstChildOfClass'PlayerGui'
+						scripts.Disabled = false
+					end
+				end
+			end
+		end
+	end,
+})
+
+--[[ Return Core ]]
+
+return Meta
+
+--[[ Module End ]]
